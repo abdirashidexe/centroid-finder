fb3a1a57e29ffa0075e7ae39ae4bdf752a9c3c53
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-12 18:24:44 -0500

    fixed illegal argument exception

--- src/DfsBinaryGroupFinder.java
+++ src/DfsBinaryGroupFinder.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class DfsBinaryGroupFinder implements BinaryGroupFinder {
@@ -32,17 +33,19 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
     * @return the found groups of connected pixels in descending order
     */
     @Override
-    public List<Group> findConnectedGroups(int[][] image) {
+    public List<Group> findConnectedGroups(int[][] image) {        
         if (image == null || image.length == 0) {
             throw new NullPointerException("the array or any of its subarrays are null");
         }
         if (image.length < 0 || image[0].length < 0) {
-            throw new IllegalArgumentException("the array or any of its subarrays are null");
+            throw new IllegalArgumentException("The array is invalid");
         }
+        List<Group> connectedPixels = new ArrayList<>();
+
         // Assuming we're at (row:0, column:0), or (0,0), then:
         // Valid: Y increases downward and X increases to the right.
         // Therefore, invalid: Y goes up from 0, X goes left from 0
-        return null;
+        return connectedPixels;
     }
     
 }
