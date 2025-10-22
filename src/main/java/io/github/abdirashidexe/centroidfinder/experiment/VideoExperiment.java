344de2dbe1072a6dffc3742263a33f7e752cb8db
Abdi Ahmed <89213120+abdirashidexe@users.noreply.github.com>
2025-10-22 15:01:05 -0700

    added butterfly vid

---
+++ src/main/java/io/github/abdirashidexe/centroidfinder/experiment/VideoExperiment.java
@@ -0,0 +1,8 @@
+package main.java.io.github.abdirashidexe.centroidfinder.experiment;
+
+import java.io.File;
+
+public class VideoExperiment {
+    File videoFile = new File("src/main/resources/videos/butterfly.mp4");
+
+}
