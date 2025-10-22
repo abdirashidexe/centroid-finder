9c976bc490b76a389fc76f2a3f35b4adc2b59f25
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-22 17:22:42 -0500

    playing with jcodec classes

--- src/main/java/io/github/abdirashidexe/centroidfinder/experiment/VideoExperiment.java
+++ src/main/java/io/github/abdirashidexe/centroidfinder/experiment/VideoExperiment.java
@@ -1,8 +1,17 @@
 package main.java.io.github.abdirashidexe.centroidfinder.experiment;
 
-import java.io.File;
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.Picture;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 public class VideoExperiment {
     File videoFile = new File("src/main/resources/videos/butterfly.mp4");
 
+    FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(videoFile));    
+    
 }
