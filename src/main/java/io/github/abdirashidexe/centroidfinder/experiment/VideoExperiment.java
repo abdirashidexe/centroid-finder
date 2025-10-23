5dca9be1280f699ef2307bd0bbeca8de88c65027
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-10-23 10:40:55 -0700

    fixed grab

--- src/main/java/io/github/abdirashidexe/centroidfinder/experiment/VideoExperiment.java
+++ src/main/java/io/github/abdirashidexe/centroidfinder/experiment/VideoExperiment.java
@@ -1,17 +1,27 @@
-package main.java.io.github.abdirashidexe.centroidfinder.experiment;
+package io.github.abdirashidexe.centroidfinder.experiment;
 
 import org.jcodec.api.FrameGrab;
 import org.jcodec.api.JCodecException;
 import org.jcodec.common.io.NIOUtils;
 import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
+
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-public class VideoExperiment {
-    File videoFile = new File("src/main/resources/videos/butterfly.mp4");
 
-    FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(videoFile));    
-    
+public class VideoExperiment {
+    public static void main(String[] args) throws IOException, JCodecException {
+        File videoFile = new File("src/main/resources/videos/butterfly.mp4");
+
+        FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(videoFile));
+        Picture picture = grab.getNativeFrame();
+
+        BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
+        ImageIO.write(bufferedImage, "png", new File("frame1.png"));
+
+        System.out.println("Frame extracted successfully!");
+    }
 }
