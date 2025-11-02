67e8abf94f57b23ba95dea2c0932d42488515387
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-11-02 14:42:01 -0800

    'get' works

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import fs from 'fs'; // file stystem module
 
 dotenv.config(); // loads variables from .env
 const app = express();
@@ -14,6 +15,19 @@ const JAR_PATH = process.env.JAR_PATH;
 console.log("Videos directory:", VIDEOS_DIR);
 console.log("Results directory:", RESULTS_DIR);
 console.log("Java JAR path:", JAR_PATH);
+
+app.get('/videos', (req, res) => {
+    try {
+        // read the directory, filter out anything that isn’t a video file (optional), & send JSON response
+        const files = fs.readdirSync(VIDEOS_DIR);
+        const videoFiles = files.filter(file => file.endsWith('.mp4'));
+        res.json({ videos: videoFiles });
+    } catch (err) {
+        // if folder doesn't exist or some error happens
+        console.error(err);
+        res.status(500).json({ error: 'Could not read videos folder' });
+    }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
