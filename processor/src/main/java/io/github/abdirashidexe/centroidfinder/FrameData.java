b1099a3bb60820d5c5b88367cee2c920047c6973
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-11-02 14:26:26 -0800

    t

--- processor/src/main/java/io/github/abdirashidexe/centroidfinder/FrameData.java
+++ processor/src/main/java/io/github/abdirashidexe/centroidfinder/FrameData.java
@@ -1,7 +1,3 @@
 package io.github.abdirashidexe.centroidfinder;
 
 public record FrameData(double time, int x, int y) {}
-<<<<<<< HEAD
-=======
-//.
->>>>>>> bbb1d3ac00cc40df60073b5ec4f97c978f12155b
