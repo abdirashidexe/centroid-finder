d9355c9522464d7c16f7f65ca017417666743857
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-11-23 16:30:33 -0800

    added javadoc to videoProcessor

--- processor/src/main/java/io/github/abdirashidexe/centroidfinder/VideoProcessor.java
+++ processor/src/main/java/io/github/abdirashidexe/centroidfinder/VideoProcessor.java
@@ -12,13 +12,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Extracts all frames from a video file as BufferedImages.
+ */
 public class VideoProcessor {
     private final String videoPath;
 
+    /**
+     * Constructs a VideoProcessor for the given video file path.
+     *
+     * @param videoPath path to the video file
+     */
     public VideoProcessor(String videoPath) {
         this.videoPath = videoPath;
     }
 
+    /**
+     * Reads a video file and returns a list of BufferedImages representing each frame.
+     *
+     * @return list of frames from the video
+     * @throws IOException if the file cannot be read
+     * @throws JCodecException if decoding fails
+     */
     public List<BufferedImage> extractFrames() throws IOException, JCodecException {
         List<BufferedImage> frames = new ArrayList<>();
         FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(new File(videoPath)));
